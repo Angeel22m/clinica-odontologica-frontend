8609c8adb29d243044ed4c2d979cd015a0183fc2
David Destephen <destephendavid@gmail.com>
2025-10-31 04:00:06 -0600

    se agrego la ruta /login

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,12 +4,14 @@ import ExpedientesPage from '../pages/ExpedientePage';
 import DashboardPage from '../pages/DashboardPage';
 import NotFoundPage from '../pages/NotFoundPage';
 import AdminServicesPage from '../pages/AdminServicesPage';
+import LoginPage from '../pages/LoginPage';
 
 const AppRoutes: React.FC = () => {
   return (
     
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/expedientes" element={<ExpedientesPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/admin/services" element={<AdminServicesPage />} />
